0345a280c3cd0ba812b6c309e792d5e8b46af4b0
domanoz <mikolaj-domanski@wp.pl>
2019-11-26 21:08:16 +0100

    Adding error handling

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
 const app = express();
 
 if (process.env.NODE_ENV === 'development') {
@@ -8,5 +12,18 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+//app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+//app.use(globalErrorHandler);
 
 module.exports = app;
