6823ccc499eaedc7ab021e0a7809ef63ac556f1a
domanoz <mikolaj-domanski@wp.pl>
2019-11-26 21:52:54 +0100

    Creating user schema and model

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,34 @@
 const mongoose = require('mongoose');
-const slugify = require('slugify');
+const validator = require('validator');
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Without name we wont know who you are !']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide youe email!'],
+    unique: true,
+    lowercase: true,
+    validator: [validator.isEmail, 'Please correct your email!']
+  },
+  avatar: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password!'],
+    minlength: 8
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, 'Please confirm your password!']
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const User = new mongoose.model('User', userSchema);
+
+module.exports = User;
