54a455ab26431c396f96b19fdfe50673ebc3f457
domanoz <mikolaj-domanski@wp.pl>
2019-11-26 21:32:48 +0100

    Handling unhandled rejections and uncaught exceptions

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,5 +1,21 @@
 const AppError = require('./../utils/appError');
 
+const handleCastErrorDB = err => {
+  return new AppError(`Invalid ${err.path}: ${err.value}`, 400);
+};
+
+const handleDuplicateFieldsDB = err => {
+  return new AppError(
+    `Deduplicated fields ${err.errmsg.match(/(["'])(\\?.)*?\1/)[0]}`
+  );
+};
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendDevError = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -24,6 +40,7 @@ const sendProdError = (err, res) => {
     });
   }
 };
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
@@ -31,6 +48,13 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendDevError(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendProdError(err, res);
+    let error = { ...err };
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+
+    sendProdError(error, res);
   }
 };
