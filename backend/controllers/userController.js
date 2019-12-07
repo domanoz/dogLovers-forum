6d38e5187d45bec5a431ed954e339fd77d3c8e4e
domanoz <mikolaj-domanski@wp.pl>
2019-12-07 23:26:08 +0100

    Working on changing data by auth user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,14 @@ const catchAsync = require('./../utils/catchAsync');
 const User = require('./../models/userModel');
 const AppError = require('./../utils/appError');
 
+exports.updateDataForAuthUser = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  if (req.user.password || req.user.confirmPassword) {
+    return next(new AppError('You cant change password here!', 400));
+  }
+});
+
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
   //console.log(req.headers);
